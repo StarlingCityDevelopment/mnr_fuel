90f0969041ff0e7c776663b366993a5452154dd8
IlMelons <melonss2002@gmail.com>
2025-05-22 20:07:31 +0200

    Unified code

--- server/server.lua
+++ server/server.lua
@@ -1,5 +1,39 @@
 Config = lib.load("config.config")
 
+---@description Fuel price init
+GlobalState:set("fuelPrice", Config.FuelPrice, true)
+
+---@description InStation Check
+local InStation = {}
+
+RegisterNetEvent("mnr_fuel:server:EnterStation", function(name)
+    local station = Config.GasStations[name]
+
+    if not source or not station then return end
+    
+    local stationCoords = station.coords
+    local stationRadius = station.radius
+    local playerPed = GetPlayerPed(source)
+    local playerCoords = GetEntityCoords(playerPed)
+
+    if #(playerCoords - stationCoords) > stationRadius then return end
+
+    InStation[source] = name
+end)
+
+RegisterNetEvent("mnr_fuel:server:ExitStation", function()
+    if not source then return end
+
+    InStation[source] = nil
+end)
+
+lib.callback.register("mnr_fuel:server:InStation", function(name)
+    if not source then return end
+    
+    return InStation[source] == name
+end)
+
+---@description Payment Handling
 lib.callback.register("mnr_fuel:server:GetPlayerMoney", function(source)
 	local cashMoney, bankMoney = server.GetPlayerMoney(source)
 
